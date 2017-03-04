bf2f9bacadc7382090c87a005f61fae4f47e4eef
Murad Shawakfa <murad.shawakfa@outlook.com>
2017-03-04 07:48:51 +0200

    Added the Helper Code

--- TrelloConnector/TrelloHelper.cs
+++ TrelloConnector/TrelloHelper.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sitecore;
+using TrelloNet;
+using TrelloConnector.Utilities;
 
 namespace TrelloConnector
 {
     public class TrelloHelper
     {
+        public void GetTrelloItems(string token)
+        {
+            //Key used by Trello to connect to our app
+            var apiKey = Sitecore.Configuration.Settings.GetSetting(Utilities.Constants.ApiKey);
+            //Name of our Trello application
+            var appName = Sitecore.Configuration.Settings.GetSetting(Utilities.Constants.AppName);
+            //initializing the connection with Trello using the App key and the user token
+            ITrello trello = new Trello(apiKey);            
+            trello.Authorize(token);
+            Board mainBoard = trello.Boards.ForMe().DefaultIfEmpty().FirstOrDefault();
+            IEnumerable<TrelloNet.List> mainLists = trello.Lists.ForBoard(mainBoard);
+            foreach(TrelloNet.List list in mainLists)
+            {
+                IEnumerable<TrelloNet.Card> cards = trello.Cards.ForList(list);
+                foreach (Card card in cards)
+                {
+                    
+                }
+            }
+            
+        }
     }
 }
